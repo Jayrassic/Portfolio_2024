aaf4d698062110c998046ced2021544ff7c2e801
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-04-05 15:24:18 -0400

    Fixed scroll centering error. Added button components

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -2,6 +2,7 @@ import { motion, useInView } from "framer-motion";
 import { useRef, useEffect } from "react";
 import emailSVG from "../assets/SVG/email.svg";
 import downloadSVG from "../assets/SVG/download.svg";
+import MotionButton from "../components/MotionButton";
 
 const Contact = () => {
   const divRef = useRef(null);
@@ -12,38 +13,39 @@ const Contact = () => {
     divRef.current.scroll(top);
   }, [isInView]);
   return (
-    <motion.div
-      ref={divRef}
-      className="flex min-h-screen snap-start flex-col overflow-y-scroll p-8"
-      initial={{ opacity: 0, x: 500 }}
-      whileInView={{ opacity: 1, x: 0 }}
-      transition={{ type: "tween", delay: 0.2, duration: 0.5 }}
-      viewport={{ amount: 0.1 }}
-    >
-      <h2 className="mb-8 rounded-md border-2 border-solid border-black p-2 text-center font-serif text-4xl">
-        Contact
-      </h2>
-
-      <div className="m-auto flex flex-col items-center gap-12 font-mono">
-        <h3>Looking for a Full-Stack web developer?</h3>
+    <div className="flex max-h-screen min-h-screen snap-start flex-col overflow-y-scroll p-8">
+      <motion.div
+        ref={divRef}
+        // className="flex min-h-screen snap-start flex-col overflow-y-scroll p-8"
+        initial={{ opacity: 0, x: 500 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ type: "tween", delay: 0.2, duration: 0.5 }}
+        viewport={{ amount: 0.1 }}
+      >
+        <h2 className="mb-8 rounded-md border-2 border-solid border-black p-2 text-center font-serif text-4xl">
+          Contact
+        </h2>
+      </motion.div>
+      <motion.div
+        ref={divRef}
+        className="m-auto flex flex-col items-center gap-12 font-mono"
+        // className="flex min-h-screen snap-start flex-col overflow-y-scroll p-8"
+        initial={{ opacity: 0, x: -500 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ type: "tween", delay: 0.2, duration: 0.5 }}
+        viewport={{ amount: 0.1 }}
+      >
+        {/* <div className="m-auto flex flex-col items-center gap-12 font-mono"> */}
+        <h3 className="text-xl">Looking for a Full-Stack web developer?</h3>
         <div>
-          <button className=" bg-blue-200">
-            Email Me
-            <img className="inline px-2" src={emailSVG} alt="email icon" />
-          </button>
+          <MotionButton text="Email Me" svg={emailSVG} />
         </div>
         <div>
-          <button>
-            Download my resume
-            <img
-              className="inline px-2"
-              src={downloadSVG}
-              alt="download icon"
-            />
-          </button>
+          <MotionButton text="Resume" svg={downloadSVG} />
         </div>
-      </div>
-    </motion.div>
+        {/* </div> */}
+      </motion.div>
+    </div>
   );
 };
 
