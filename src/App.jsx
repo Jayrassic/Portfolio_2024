62ebb0609f9f55922b917435298ad2db9c39e9e5
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-04-07 20:50:01 -0400

    Added react-intersection-observer to change nav styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,15 @@ import ClientProjects from "./views/ClientProjects";
 import Contact from "./views/Contact";
 import githubSVG from "./assets/SVG/github.svg";
 import emailSVG from "./assets/SVG/email.svg";
+import { useInView } from "react-intersection-observer";
 
 function App() {
+  const [homeRef, homeVis] = useInView({ threshold: 0.5 });
+  const [aboutRef, aboutVis] = useInView({ threshold: 0.5 });
+  const [personalRef, personalVis] = useInView({ threshold: 0.5 });
+  const [clientRef, clientVis] = useInView({ threshold: 0.5 });
+  const [contactRef, contactVis] = useInView({ threshold: 0.5 });
+
   function scrollHandler(e) {
     e.preventDefault();
     const target = e.target;
@@ -22,7 +29,11 @@ function App() {
     <div className="grid grid-cols-10 bg-slate-100">
       <nav className="font-figtree sticky col-span-2 flex h-screen flex-col justify-evenly self-start bg-emerald-400 p-3 text-center">
         <h1
-          className="m-1 p-1 text-4xl font-extrabold text-slate-100"
+          className={
+            homeVis
+              ? "m-1 p-1 text-4xl font-extrabold text-slate-100"
+              : "m-1 p-1 text-4xl font-extrabold"
+          }
           onClick={(e) => scrollHandler(e)}
         >
           <a href="#home">Jason Gaglio</a>
@@ -32,27 +43,39 @@ function App() {
           onClick={(e) => scrollHandler(e)}
         >
           <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
-            <a className="text-2xl" href="#about">
+            <a
+              className={aboutVis ? "text-2xl text-slate-100" : "text-2xl"}
+              href="#about"
+            >
               About Me
             </a>
           </motion.li>
-          <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
+          {/* <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
             <a className="text-2xl" href="#">
               Skills
             </a>
-          </motion.li>
+          </motion.li> */}
           <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
-            <a className="text-2xl" href="#personal">
+            <a
+              className={personalVis ? "text-2xl text-slate-100" : "text-2xl"}
+              href="#personal"
+            >
               Personal Projects
             </a>
           </motion.li>
           <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
-            <a className="text-2xl" href="#client">
+            <a
+              className={clientVis ? "text-2xl text-slate-100" : "text-2xl"}
+              href="#client"
+            >
               Client Projects
             </a>
           </motion.li>
           <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.12 }}>
-            <a className="text-2xl" href="#contact">
+            <a
+              className={contactVis ? "text-2xl text-slate-100" : "text-2xl"}
+              href="#contact"
+            >
               Contact
             </a>
           </motion.li>
@@ -76,19 +99,39 @@ function App() {
       </nav>
 
       <div className="font-figtree col-span-8 max-h-screen snap-y snap-mandatory scroll-py-0 overflow-x-hidden overflow-y-scroll">
-        <div id="home" className="min-h-screen snap-start snap-always">
+        <div
+          ref={homeRef}
+          id="home"
+          className="min-h-screen snap-start snap-always"
+        >
           <Home />
         </div>
-        <div id="about" className="min-h-screen snap-start snap-always">
+        <div
+          ref={aboutRef}
+          id="about"
+          className="min-h-screen snap-start snap-always"
+        >
           <AboutMe />
         </div>
-        <div id="personal" className="min-h-screen snap-start snap-always">
+        <div
+          ref={personalRef}
+          id="personal"
+          className="min-h-screen snap-start snap-always"
+        >
           <PersonalProjects />
         </div>
-        <div id="client" className=" h-screen snap-start snap-always">
+        <div
+          ref={clientRef}
+          id="client"
+          className=" h-screen snap-start snap-always"
+        >
           <ClientProjects />
         </div>
-        <div id="contact" className=" h-screen snap-start snap-always">
+        <div
+          ref={contactRef}
+          id="contact"
+          className=" h-screen snap-start snap-always"
+        >
           <Contact />
         </div>
       </div>
