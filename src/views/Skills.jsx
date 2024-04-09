208932404a25fb308f212ca7e53432bf8d5d951c
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-04-09 13:38:05 -0400

    Created skills view

---
+++ src/views/Skills.jsx
@@ -0,0 +1,58 @@
+import { motion, useInView } from "framer-motion";
+import { useEffect, useRef } from "react";
+import {
+  mainVariant,
+  childFromLeftVariant,
+  childFromRightVariant,
+} from "../helpers/viewVariants";
+import {
+  FrontendSkillsSVGs,
+  BackendSkillsSVGs,
+} from "../components/SkillsSVGs";
+
+const Skills = () => {
+  const divRef = useRef(null);
+
+  const isInView = useInView(divRef, { margin: "-1px" });
+
+  useEffect(() => {
+    divRef.current.scroll(top);
+  }, [isInView]);
+
+  return (
+    <motion.div
+      variants={mainVariant}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ amount: 0.1 }}
+      ref={divRef}
+      className="no-scrollbar flex w-full snap-start  flex-col overflow-x-hidden overflow-y-scroll p-4 sm:max-h-screen sm:min-h-screen sm:p-8"
+    >
+      <motion.div variants={childFromRightVariant}>
+        <h2 className="rounded-md border-2 border-solid border-black bg-emerald-400 p-2 text-center text-4xl font-bold ">
+          Skills
+        </h2>
+      </motion.div>
+
+      <motion.div
+        className=" flex w-full flex-grow flex-col gap-4 p-4 text-lg sm:m-auto sm:justify-around "
+        variants={childFromLeftVariant}
+      >
+        <div className="flex flex-col items-center text-center">
+          <h3 className="m-4 rounded-md border-2 border-solid border-black bg-emerald-400 p-2 text-center text-3xl font-bold">
+            Frontend:
+          </h3>
+          <FrontendSkillsSVGs />
+        </div>
+        <div className="flex flex-col items-center text-center">
+          <h3 className="m-4 rounded-md border-2 border-solid border-black bg-emerald-400 p-2 text-center text-3xl font-bold">
+            Backend:
+          </h3>
+          <BackendSkillsSVGs />
+        </div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Skills;
