80611af6529235325407ad43d8e9785c082c2b50
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-04-09 13:37:34 -0400

    Created skills SVG components

---
+++ src/components/SkillsSVGs.jsx
@@ -0,0 +1,119 @@
+import { motion } from "framer-motion";
+
+import javascriptSVG from "../assets/SVG/javascript.svg";
+import bootstrapSVG from "../assets/SVG/bootstrap.svg";
+import typescriptSVG from "../assets/SVG/typescript.svg";
+import reactSVG from "../assets/SVG/react.svg";
+import tailwindSVG from "../assets/SVG/tailwind.svg";
+
+import expressSVG from "../assets/SVG/express.svg";
+import jwtSVG from "../assets/SVG/JWT.svg";
+import mongoSVG from "../assets/SVG/mongodb.svg";
+import nodeSVG from "../assets/SVG/node.svg";
+import ejsSVG from "../assets/SVG/ejs.svg";
+
+const mainVariant = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.25,
+      when: "beforeChildren",
+    },
+  },
+};
+
+const childFromRightVariant = {
+  hidden: { opacity: 0, x: "110vw" },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { type: "spring", duration: 0.5 },
+  },
+};
+
+export const FrontendSkillsSVGs = () => {
+  return (
+    <motion.div
+      variants={mainVariant}
+      initial="hidden"
+      whileInView="visible"
+      className="flex w-full justify-evenly"
+    >
+      <motion.img
+        variants={childFromRightVariant}
+        src={javascriptSVG}
+        width="100px"
+        alt="javascript svg"
+      />
+      <motion.img
+        variants={childFromRightVariant}
+        src={typescriptSVG}
+        width="100px"
+        alt="typescript svg"
+      />
+      <motion.img
+        variants={childFromRightVariant}
+        src={tailwindSVG}
+        width="100px"
+        alt="tailwind svg"
+      />
+      <motion.img
+        variants={childFromRightVariant}
+        src={bootstrapSVG}
+        width="100px"
+        alt="bootstrap svg"
+      />
+      <motion.img
+        variants={childFromRightVariant}
+        src={reactSVG}
+        width="100px"
+        alt="react svg"
+      />
+    </motion.div>
+  );
+};
+
+export const BackendSkillsSVGs = () => {
+  return (
+    <motion.div
+      variants={mainVariant}
+      initial="hidden"
+      whileInView="visible"
+      className="flex w-full justify-evenly"
+    >
+      <motion.img
+        variants={childFromRightVariant}
+        src={nodeSVG}
+        width="100px"
+        alt="node svg"
+      />
+      <motion.img
+        variants={childFromRightVariant}
+        src={expressSVG}
+        width="100px"
+        alt="express svg"
+      />
+      <motion.img
+        variants={childFromRightVariant}
+        src={mongoSVG}
+        width="100px"
+        alt="mongoDB svg"
+      />
+      <motion.img
+        variants={childFromRightVariant}
+        src={jwtSVG}
+        width="100px"
+        alt="JWT svg"
+      />
+      <motion.img
+        variants={childFromRightVariant}
+        src={ejsSVG}
+        width="100px"
+        alt="EJS svg"
+      />
+    </motion.div>
+  );
+};
