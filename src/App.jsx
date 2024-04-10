286608b828acaa6222d49f572bbfdea1356728d3
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-04-10 09:55:01 -0400

    Removed nav from App.jsx and placed into Nav component. Nav component inserted into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,112 +1,60 @@
-import { motion } from "framer-motion";
 import Home from "./views/Home";
 import AboutMe from "./views/AboutMe";
 import Skills from "./views/Skills";
 import PersonalProjects from "./views/PersonalProjects";
 import ClientProjects from "./views/ClientProjects";
 import Contact from "./views/Contact";
-import githubSVG from "./assets/SVG/github.svg";
-import emailSVG from "./assets/SVG/email.svg";
 import { useInView } from "react-intersection-observer";
+import Nav from "./components/Nav";
 
 function App() {
-  const [homeRef, homeVis] = useInView({ threshold: 0.5 });
-  const [aboutRef, aboutVis] = useInView({ threshold: 0.5 });
-  const [skillsRef, skillsVis] = useInView({ threshold: 0.5 });
-  const [personalRef, personalVis] = useInView({ threshold: 0.5 });
-  const [clientRef, clientVis] = useInView({ threshold: 0.5 });
-  const [contactRef, contactVis] = useInView({ threshold: 0.5 });
+  const [homeRef, homeViewState] = useInView({ threshold: 0.5 });
+  const [aboutRef, aboutViewState] = useInView({ threshold: 0.5 });
+  const [skillsRef, skillsViewState] = useInView({ threshold: 0.5 });
+  const [personalRef, personalViewState] = useInView({ threshold: 0.5 });
+  const [clientRef, clientViewState] = useInView({ threshold: 0.5 });
+  const [contactRef, contactViewState] = useInView({ threshold: 0.5 });
 
-  function scrollHandler(e) {
-    e.preventDefault();
-    const target = e.target;
-    const id = target.getAttribute("href").replace("#", "");
-    const element = document.getElementById(id);
-    element.scrollIntoView(true, {
-      behavior: "smooth",
-    });
-  }
+  const stateArray = [
+    homeViewState,
+    aboutViewState,
+    skillsViewState,
+    personalViewState,
+    clientViewState,
+    contactViewState,
+  ];
+
+  const inViewObject = {
+    home: {
+      ref: homeRef,
+      state: homeViewState,
+    },
+    about: {
+      ref: aboutRef,
+      state: aboutViewState,
+    },
+    skills: {
+      ref: skillsRef,
+      state: skillsViewState,
+    },
+    personal: {
+      ref: personalRef,
+      state: personalViewState,
+    },
+    client: {
+      ref: clientRef,
+      state: clientViewState,
+    },
+    contact: {
+      ref: contactRef,
+      state: contactViewState,
+    },
+  };
 
   return (
-    <div className=" max-h-screen min-h-screen bg-slate-100 sm:grid sm:grid-cols-10">
-      <nav
-        className="font-figtree flex h-20 justify-evenly bg-emerald-400 p-1 text-center sm:sticky sm:top-0 sm:col-span-2 sm:h-screen sm:flex-col sm:p-3
-        "
-      >
-        <h1
-          className={
-            homeVis
-              ? "m-1 self-center p-1 text-4xl font-extrabold text-slate-100"
-              : "m-1 self-center p-1 text-4xl font-extrabold"
-          }
-          onClick={(e) => scrollHandler(e)}
-        >
-          <a href="#home">Jason Gaglio</a>
-        </h1>
-        <motion.ul
-          className="my-8 hidden flex-col items-center gap-4 text-center font-bold text-neutral-800 sm:flex "
-          onClick={(e) => scrollHandler(e)}
-        >
-          <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
-            <a
-              className={aboutVis ? "text-2xl text-slate-100" : "text-2xl"}
-              href="#about"
-            >
-              About Me
-            </a>
-          </motion.li>
-          <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
-            <a
-              className={skillsVis ? "text-2xl text-slate-100" : "text-2xl"}
-              href="#"
-            >
-              Skills
-            </a>
-          </motion.li>
-          <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
-            <a
-              className={personalVis ? "text-2xl text-slate-100" : "text-2xl"}
-              href="#personal"
-            >
-              Personal Projects
-            </a>
-          </motion.li>
-          <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
-            <a
-              className={clientVis ? "text-2xl text-slate-100" : "text-2xl"}
-              href="#client"
-            >
-              Client Projects
-            </a>
-          </motion.li>
-          <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.12 }}>
-            <a
-              className={contactVis ? "text-2xl text-slate-100" : "text-2xl"}
-              href="#contact"
-            >
-              Contact
-            </a>
-          </motion.li>
-        </motion.ul>
-        <div className="hidden justify-evenly sm:flex">
-          <a href="#">
-            <motion.img
-              whileHover={{ x: 4, scale: 1.12 }}
-              src={githubSVG}
-              alt="github logo"
-            />
-          </a>
-          <a href="#">
-            <motion.img
-              whileHover={{ x: 4, scale: 1.12 }}
-              src={emailSVG}
-              alt="mail envelop"
-            />
-          </a>
-        </div>
-      </nav>
-
-      <div
+    <main className=" max-h-screen min-h-screen bg-slate-100 sm:grid sm:grid-cols-10">
+      <Nav viewObject={inViewObject} />
+      <section
         className={`font-figtree h-[calc(100vh-5rem)] snap-y snap-mandatory scroll-p-0 overflow-x-hidden overflow-y-scroll sm:col-span-8 sm:max-h-screen sm:min-h-screen`}
       >
         <div
@@ -152,8 +100,8 @@ function App() {
         >
           <Contact />
         </div>
-      </div>
-    </div>
+      </section>
+    </main>
   );
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,103 @@
-const Nav = () => {
+import { motion } from "framer-motion";
+import PropTypes from "prop-types";
+import githubSVG from "../assets/SVG/github.svg";
+import emailSVG from "../assets/SVG/email.svg";
+
+const Nav = ({ viewObject }) => {
+  function scrollHandler(e) {
+    e.preventDefault();
+    const target = e.target;
+    const id = target.getAttribute("href").replace("#", "");
+    const element = document.getElementById(id);
+    element.scrollIntoView(true, {
+      behavior: "smooth",
+    });
+  }
+
+  const { home, about, skills, personal, client, contact } = viewObject;
+
   return (
-    <nav className="col-span-2 bg-green-700 h-screen">
-      <h1>Jason Gaglio</h1>
-      <ul>
-        <li>
-          <a href="#">about</a>
-          <a href="#">Personal Projects</a>
-          <a href="#">Other Link</a>
-        </li>
-      </ul>
+    <nav
+      className="font-figtree flex h-20 justify-evenly bg-emerald-400 p-1 text-center sm:sticky sm:top-0 sm:col-span-2 sm:h-screen sm:flex-col sm:p-3
+    "
+    >
+      <h1
+        className={
+          home.state
+            ? "m-1 self-center p-1 text-4xl font-extrabold text-slate-100"
+            : "m-1 self-center p-1 text-4xl font-extrabold"
+        }
+        onClick={(e) => scrollHandler(e)}
+      >
+        <a href="#home">Jason Gaglio</a>
+      </h1>
+      <motion.ul
+        className="my-8 hidden flex-col items-center gap-4 text-center font-bold text-neutral-800 sm:flex "
+        onClick={(e) => scrollHandler(e)}
+      >
+        <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
+          <a
+            className={about.state ? "text-2xl text-slate-100" : "text-2xl"}
+            href="#about"
+          >
+            About Me
+          </a>
+        </motion.li>
+        <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
+          <a
+            className={skills.state ? "text-2xl text-slate-100" : "text-2xl"}
+            href="#"
+          >
+            Skills
+          </a>
+        </motion.li>
+        <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
+          <a
+            className={personal.state ? "text-2xl text-slate-100" : "text-2xl"}
+            href="#personal"
+          >
+            Personal Projects
+          </a>
+        </motion.li>
+        <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
+          <a
+            className={client.state ? "text-2xl text-slate-100" : "text-2xl"}
+            href="#client"
+          >
+            Client Projects
+          </a>
+        </motion.li>
+        <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.12 }}>
+          <a
+            className={contact.state ? "text-2xl text-slate-100" : "text-2xl"}
+            href="#contact"
+          >
+            Contact
+          </a>
+        </motion.li>
+      </motion.ul>
+      <div className="hidden justify-evenly sm:flex">
+        <a href="#">
+          <motion.img
+            whileHover={{ x: 4, scale: 1.12 }}
+            src={githubSVG}
+            alt="github logo"
+          />
+        </a>
+        <a href="#">
+          <motion.img
+            whileHover={{ x: 4, scale: 1.12 }}
+            src={emailSVG}
+            alt="mail envelop"
+          />
+        </a>
+      </div>
     </nav>
   );
 };
 
 export default Nav;
+
+Nav.propTypes = {
+  viewObject: PropTypes.object.isRequired,
+};
