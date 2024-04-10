8adfbfeabbe83e096b412a9c1be5474c5c6ac436
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-04-10 16:05:02 -0400

    Made responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,99 +1,120 @@
 import { motion } from "framer-motion";
-import PropTypes from "prop-types";
+import { useState } from "react";
 import githubSVG from "../assets/SVG/github.svg";
 import emailSVG from "../assets/SVG/email.svg";
+import PropTypes from "prop-types";
+import HamburgerButton from "./Hamburger";
 
 const Nav = ({ viewObject }) => {
   const { home, about, skills, personal, client, contact } = viewObject;
+
+  const [showMenu, setShowMenu] = useState(false);
+
+  let needMenu = window.screen.width <= 640;
+
+  window.addEventListener("resize", () =>
+    setShowMenu(window.screen.width <= 640),
+  );
+
+  window.addEventListener("orientationchange", () =>
+    setShowMenu(window.screen.width <= 640),
+  );
 
   function scrollHandler(e) {
     e.preventDefault();
     const target = e.target;
     const id = target.getAttribute("href").replace("#", "");
     const element = document.getElementById(id);
+    setShowMenu(false);
     element.scrollIntoView(true, {
       behavior: "smooth",
     });
   }
 
   return (
-    <nav
-      className="font-figtree flex h-20 justify-evenly bg-emerald-400 p-1 text-center sm:sticky sm:top-0 sm:col-span-2 sm:h-screen sm:flex-col sm:p-3
-    "
-    >
-      <h1
-        className={
-          home.state
-            ? "m-1 self-center p-1 text-4xl font-extrabold text-slate-100"
-            : "m-1 self-center p-1 text-4xl font-extrabold"
-        }
-        onClick={(e) => scrollHandler(e)}
-      >
-        <a href="#home">Jason Gaglio</a>
-      </h1>
-      <motion.ul
-        className="my-8 hidden flex-col items-center gap-4 text-center font-bold text-neutral-800 sm:flex "
-        onClick={(e) => scrollHandler(e)}
-      >
-        <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
-          <a
-            className={about.state ? "text-2xl text-slate-100" : "text-2xl"}
-            href="#about"
-          >
-            About Me
-          </a>
-        </motion.li>
-        <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
-          <a
-            className={skills.state ? "text-2xl text-slate-100" : "text-2xl"}
-            href="#"
-          >
-            Skills
-          </a>
-        </motion.li>
-        <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
-          <a
-            className={personal.state ? "text-2xl text-slate-100" : "text-2xl"}
-            href="#personal"
-          >
-            Personal Projects
-          </a>
-        </motion.li>
-        <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
-          <a
-            className={client.state ? "text-2xl text-slate-100" : "text-2xl"}
-            href="#client"
-          >
-            Client Projects
-          </a>
-        </motion.li>
-        <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.12 }}>
-          <a
-            className={contact.state ? "text-2xl text-slate-100" : "text-2xl"}
-            href="#contact"
-          >
-            Contact
-          </a>
-        </motion.li>
-      </motion.ul>
-      <div className="hidden justify-evenly sm:flex">
-        <a href="#">
-          <motion.img
-            width="35px"
-            whileHover={{ x: 4, scale: 1.12 }}
-            src={githubSVG}
-            alt="github logo"
-          />
-        </a>
-        <a href="#">
-          <motion.img
-            width="35px"
-            whileHover={{ x: 4, scale: 1.12 }}
-            src={emailSVG}
-            alt="mail envelop"
-          />
-        </a>
+    <nav className="z-30 flex h-20 justify-evenly text-center font-figtree shadow-md sm:sticky sm:top-0 sm:col-span-2 sm:h-screen sm:flex-col sm:bg-emerald-400">
+      <div className="z-20 flex flex-grow items-center justify-around bg-emerald-400 p-1 shadow-md sm:flex-grow-0 sm:p-3 sm:shadow-none ">
+        <h1
+          className={`m-1 self-center p-1 text-4xl font-extrabold ${home.state ? "text-slate-100" : ""}`}
+          onClick={(e) => scrollHandler(e)}
+        >
+          <a href="#home">Jason Gaglio</a>
+        </h1>
+        <HamburgerButton state={showMenu} setState={setShowMenu} />
       </div>
+
+      <motion.div
+        className={`absolute top-[5rem] z-10 flex w-full flex-col items-end gap-3 rounded-b-lg bg-emerald-300 py-4 text-center font-bold text-neutral-800 sm:static sm:flex-grow  sm:items-center sm:justify-around sm:gap-4 sm:bg-emerald-400 sm:py-0 sm:shadow-none`}
+        initial={needMenu && { y: showMenu ? 0 : -305, opacity: 0 }}
+        animate={needMenu && { y: showMenu ? 0 : -305, opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <motion.ul
+          className={`flex flex-col items-end gap-2 px-8 py-2 text-center font-bold text-neutral-800 sm:relative sm:my-9 sm:items-center sm:gap-4 sm:bg-emerald-400 sm:p-4 sm:shadow-none`}
+          onClick={(e) => scrollHandler(e)}
+        >
+          <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
+            <a
+              className={about.state ? "text-2xl text-slate-100" : "text-2xl"}
+              href="#about"
+            >
+              About Me
+            </a>
+          </motion.li>
+          <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
+            <a
+              className={skills.state ? "text-2xl text-slate-100" : "text-2xl"}
+              href="#skills"
+            >
+              Skills
+            </a>
+          </motion.li>
+          <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
+            <a
+              className={
+                personal.state ? "text-2xl text-slate-100" : "text-2xl"
+              }
+              href="#personal"
+            >
+              Personal Projects
+            </a>
+          </motion.li>
+          <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.2 }}>
+            <a
+              className={client.state ? "text-2xl text-slate-100" : "text-2xl"}
+              href="#client"
+            >
+              Client Projects
+            </a>
+          </motion.li>
+          <motion.li className="w-fit" whileHover={{ x: 4, scale: 1.12 }}>
+            <a
+              className={contact.state ? "text-2xl text-slate-100" : "text-2xl"}
+              href="#contact"
+            >
+              Contact
+            </a>
+          </motion.li>
+        </motion.ul>
+        <div className="flex gap-4 px-8 py-2 sm:w-full sm:justify-evenly ">
+          <a href="#">
+            <motion.img
+              width="35px"
+              whileHover={{ x: 4, scale: 1.12 }}
+              src={githubSVG}
+              alt="github logo"
+            />
+          </a>
+          <a href="#">
+            <motion.img
+              width="35px"
+              whileHover={{ x: 4, scale: 1.12 }}
+              src={emailSVG}
+              alt="mail envelop"
+            />
+          </a>
+        </div>
+      </motion.div>
     </nav>
   );
 };
