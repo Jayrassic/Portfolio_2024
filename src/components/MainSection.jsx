b83409aa2d30f741319a79d2ddca5ee6d7fac5e9
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-04-12 21:36:31 -0400

    Added tab indexs

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -14,6 +14,7 @@ const MainSection = ({ viewObject }) => {
       className={`h-[calc(100svh-5rem)] snap-y snap-mandatory scroll-p-0 overflow-x-hidden overflow-y-scroll font-figtree sm:col-span-8 sm:max-h-svh sm:min-h-svh`}
     >
       <div
+        tabIndex={0}
         ref={home.ref}
         id="home"
         className={`h-full snap-start snap-always sm:max-h-svh sm:min-h-svh`}
@@ -21,6 +22,7 @@ const MainSection = ({ viewObject }) => {
         <Home />
       </div>
       <div
+        tabIndex={0}
         ref={about.ref}
         id="about"
         className={`h-full snap-start snap-always sm:max-h-svh sm:min-h-svh`}
@@ -29,6 +31,7 @@ const MainSection = ({ viewObject }) => {
       </div>
 
       <div
+        tabIndex={0}
         ref={skills.ref}
         id="skills"
         className="h-full snap-start snap-always sm:max-h-svh sm:min-h-svh"
@@ -36,6 +39,7 @@ const MainSection = ({ viewObject }) => {
         <Skills />
       </div>
       <div
+        tabIndex={0}
         ref={personal.ref}
         id="personal"
         className="h-full snap-start snap-always sm:max-h-svh sm:min-h-svh"
@@ -43,6 +47,7 @@ const MainSection = ({ viewObject }) => {
         <PersonalProjects />
       </div>
       <div
+        tabIndex={0}
         ref={client.ref}
         id="client"
         className="h-full snap-start snap-always sm:max-h-svh sm:min-h-svh"
@@ -50,6 +55,7 @@ const MainSection = ({ viewObject }) => {
         <ClientProjects />
       </div>
       <div
+        tabIndex={0}
         ref={contact.ref}
         id="contact"
         className="h-full snap-start snap-always sm:max-h-svh sm:min-h-svh"
