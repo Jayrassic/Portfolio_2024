15d30bcc134864e105a90dfc67da25034049fa28
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-04-14 13:54:26 -0400

    Created custom drop shadow

--- src/components/SkillsSVGContainer.jsx
+++ src/components/SkillsSVGContainer.jsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 const SkillsSVGContainer = ({ svgSource, alt }) => {
   return (
     <motion.img
-      className="drop-shadow-xl"
+      className="drop-shadow-[1px_3px_6px_rgb(0,0,0,0.3)]"
       variants={childFromRightVariant}
       src={svgSource}
       width="100px"
